6f179acab628335020f016d2815404d64149c75b
Netzku <167686398+NetzkuOfficial@users.noreply.github.com>
2025-01-03 23:01:38 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Menyajikan file statis dari folder public
 app.use(express.static(path.join(__dirname, '../public')));
@@ -9,6 +10,10 @@ app.use(express.static(path.join(__dirname, '../public')));
 // Menangani semua rute dengan file `search.html`
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/search.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
 });
 
 module.exports = app;
